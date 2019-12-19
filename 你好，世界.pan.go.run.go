5235e9f1374018d6696a077f65afb8c871f04f2b
youkpan <youkpan@gmail.com>
2019-12-19 18:57:56 +0800

    fix func name

--- 你好，世界.pan.go.run.go
+++ 你好，世界.pan.go.run.go
@@ -16,8 +16,8 @@ import (
 
 */
 
-var variable_1 = [] string {"世界 你好","你在哪里？","我来了 pan 语言"}
-var variable_3  map [string  ] string
+var variable_2 = [] string {"世界 你好","你在哪里？","我来了 pan 语言"}
+var variable_4  map [string  ] string
 
 func main(){
     if len(os.Args) < 2 {
@@ -28,64 +28,64 @@ func main(){
     fmt.Println(" 欢迎 使用 pan 语言,详细使用说明请查看：  https://github.com/youkpan/panlang")
     fmt.Println("")
     fmt.Println("方法1："  )
-    for variable_61 := 0 ; variable_61 <  len(variable_1); variable_61 = variable_61 + 1 {
-        fmt.Println(variable_1 [ variable_61 ] )
+    for variable_51 := 0 ; variable_51 <  len(variable_2); variable_51 = variable_51 + 1 {
+        fmt.Println(variable_2 [ variable_51 ] )
     }
     fmt.Println("")
     fmt.Println("方法2："  )
-    for variable_61 ,variable_7 := range  variable_1 {
-        fmt.Println("索引 值：",variable_61,",结果：",variable_7 )
+    for variable_51 ,variable_6 := range  variable_2 {
+        fmt.Println("索引 值：",variable_51,",结果：",variable_6 )
     }
     fmt.Println("")
     fmt.Println("方法3："  )
-    variable_3 := make(map [string  ] string)
-    variable_3 ["天王星"  ] = " 比海王星近"
-    variable_3 ["地球"  ] = " 我们.家园"
-    variable_3 = 修改variable_18(variable_3)
-    for variable_61 ,variable_7 := range  variable_3 {
-        if variable_61 == "地球" {
+    variable_4 := make(map [string  ] string)
+    variable_4 ["天王星"  ] = " 比海王星近"
+    variable_4 ["地球"  ] = " 我们.家园"
+    variable_4 = variable_13(variable_4)
+    for variable_51 ,variable_6 := range  variable_4 {
+        if variable_51 == "地球" {
             fmt.Println("发现地球"  )
-            fmt.Println("索引 值：",variable_61,",结果：",variable_7  )
+            fmt.Println("索引 值：",variable_51,",结果：",variable_6  )
         }else{
-            fmt.Println("索引 值：",variable_61,",结果：",variable_7 )
+            fmt.Println("索引 值：",variable_51,",结果：",variable_6 )
         }
     }
     fmt.Println("")
-    fmt.Println("读取文件")
-    读取文件("README.md")
+    fmt.Println("variable_15")
+    variable_15("README.md")
 
     for {
-        variable_13 := time . Now() . Format(utils.TIME_LAYOUT)
-        fmt.Println("现在是：",variable_13)
+        variable_11 := time . Now() . Format(utils.TIME_LAYOUT)
+        fmt.Println("现在是：",variable_11)
         time. Sleep( time.Duration(10)* time.Second)
     }
 }
 
-func 修改variable_18(variable_181 map [string  ] string)map [string  ] string{
-    variable_181 ["火星" ] = "要去吗"
-    return variable_181
+func variable_13 (variable_141 map [string  ] string)map [string  ] string{
+    variable_141 ["火星" ] = "要去吗"
+    return variable_141
 }
 
-func 读取文件(variable_21 string) string{
-    文件1,variable_22 := os . Open(variable_21)
-    if variable_22 != nil{
+func variable_15 (variable_16 string) string{
+    variable_191,variable_18 := os . Open(variable_16)
+    if variable_18 != nil{
         return ""
     }
 
-    reader1 := bufio . NewReader(文件1)
+    reader1 := bufio . NewReader(variable_191)
 
-    variable_24 := [] string {}
+    variable_20 := [] string {}
 
     for {
-        variable_25, variable_222 := reader1 . ReadString('\n')
-        variable_27string := string(variable_25)
+        variable_22, variable_182 := reader1 . ReadString('\n')
+        variable_24string := string(variable_22)
 
-        fmt.Println(variable_27string)
-        if variable_222 != nil{
+        fmt.Println(variable_24string)
+        if variable_182 != nil{
             break
         }
-        variable_24 =  append (variable_24 ,variable_27string)
-        return variable_27string
+        variable_20 =  append (variable_20 ,variable_24string)
+        return variable_24string
     }
 
     return ""
